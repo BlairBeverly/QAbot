cc2995cd8001b1d292fa6e549a263713a6f7411c
Blair Beverly <blair.beverly@gmail.com>
2016-08-22 16:26:00 -0700

    Add scoreboard and reset commands.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# jeopardybot
+# QuizBot
+
+Thanks to Matt Makai from Full Stack Python for a starter slack bot tutorial used on this project:
+https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
+
+
+
+
--- starterbot.py
+++ starterbot.py
@@ -3,6 +3,7 @@ import time
 import random
 from mysqlconnection import MySQLConnector
 from slackclient import SlackClient
+import re
 
 mysql = MySQLConnector()
 
@@ -10,34 +11,50 @@ mysql = MySQLConnector()
 BOT_ID = os.environ.get("BOT_ID") # starterbot's ID as an environment variable
 BOT_NAME = "starterbot"
 AT_BOT = "<@" + BOT_ID + ">"
-CATEGORIES_COMMAND = "categories" 
+QUESTIONS_COMMAND = "questions" 
 ASK_COMMAND = "I'll take"
 ANSWER_COMMAND = "answer"
+SCOREBOARD_COMMAND = "scoreboard"
+NUM_QUESTIONS = 3
+RESET_COMMAND = "reset"
+SPLIT_REGEX_PATTERN = r"[:\s]"
+NEXT_COMMAND = "next"
 
-class QuestionAnswerBot(object):
+class QuizBot(object):
     def __init__(self):
-        self.question = ''
-        self.answer = ''
+        self.current_question = ''
+        self.current_answer = ''
+        self.current_point_value = 0
+        self.scoreboard = {}
         self.default_response = "I'm ready to play a game! Say *@{} {}* to "\
-            "see a list of available question categories and their point "\
-            "values.".format(BOT_NAME, CATEGORIES_COMMAND)
+            "see a list of available questions and their point values." \
+            .format(BOT_NAME, QUESTIONS_COMMAND)
+
+        self.available_questions = random.sample(
+            self.get_all_questions(), NUM_QUESTIONS)
+
+        print self.available_questions
 
         # instantiate Slack client
         self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
-    def get_categories_response(self):
+    def get_all_questions(self):
+        all_questions = mysql.query_db("SELECT question, answer, category, "\
+                                       "point_value FROM questions")
+        return all_questions
+
+
+    def get_questions_response(self, name):
         """
         Returns a string detailing the list of available questions and 
-        how much each question is worth
+        how much each question is worth.
         """
-        query_result = mysql.query_db("SELECT category, point_value FROM "
-            "questions GROUP BY 1,2 ORDER BY point_value ASC")
         response = "Here are the avaialble question categories and point "\
-                   "values:\n'"
+                   "values, {}:\n".format(name)
 
-        for row in query_result:
+        for question in self.available_questions:
             response += "{}: {} points\n".format(
-                row['category'], row['point_value'])
+                question['category'], question['point_value'])
 
         response += ("\nUse the *{}* command followed by the category "\
             "and point value to a question. For example, '@{} I'll take "\
@@ -48,64 +65,131 @@ class QuestionAnswerBot(object):
 
     def set_question_answer(self, category, point_value):
         """
-        Queries the database to set the bot's question and answer.
+        Set the bot's question and answer.
         """
+        print category
+        print point_value
 
-        question_list = mysql.query_db(
-            "SELECT question, answer FROM questions WHERE category = '{}' "\
-            "AND point_value = {}".format(category, point_value))
+        # Choose a question with the given category and point value
+        for index, question in enumerate(self.available_questions):
+            print question
+            if question['category'].lower() == category and question['point_value'] \
+            == point_value:
+                print "match"
+                self.current_question = "{} for {}: ".format(
+                    category, point_value, question['question'])
+                self.current_answer = question['answer']
+                self.current_point_value = question['point_value']
+                del self.available_questions[index]
+                return 'ok'
 
-        # Choose a random row to get a question & answer
-        random_choice = random.choice(range(0,len(question_list)))
-        
-        self.question = question_list[random_choice]['question']
-        self.answer = question_list[random_choice]['answer']
 
-
-    def handle_command(self, command, channel):
+    def handle_command(self, command, channel, user):
         """
         Receives commands directed at the bot and determines if they
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
         """
+
+        api_call = self.slack_client.api_call("users.info", user=user)
+        if api_call.get('ok'):
+            name = api_call.get("user")['profile']['first_name']
+
         print "user command:", command
 
         response = self.default_response
 
-        if command.lower().startswith(ASK_COMMAND.lower()):
-            if self.question:
-                response = "I've already asked a question. Answer that one "\
-                           "first and then I can give you another."
+        if command.lower().startswith(RESET_COMMAND.lower()):
+            self.available_questions = random.sample(
+                self.get_all_questions(), NUM_QUESTIONS)
+            self.scoreboard = {}
+
+            response = "Questions and scores are reset! Tell me '{}' to see the "\
+                       "available questions and their point values"\
+                       .format(QUESTIONS_COMMAND)
+
+        elif command.lower().startswith(ASK_COMMAND.lower()):
+            if self.current_question:
+                response = "I've already asked a question, {}. Answer that one "\
+                           "first and then I can ask another.".format(name)
             else:
                 # Assuming command looks something like: 'I'll take Python 100'
-                try:
-                    category, point_value = command[9:].split()
-                except ValueError:
-                    response = "Give me a command that looks something like: "\
-                    "'I'll take Python 100'"
+                if len(self.available_questions) == 0:
+                    response = "There are no more questions! Tell me 'reset' "\
+                               "to play another game."
 
-                self.set_question_answer(category, point_value)
-                response = self.question
+                else:
+                    try:
+                        category, point_value = re.split(
+                            SPLIT_REGEX_PATTERN, command[len(ASK_COMMAND):].strip(), maxsplit=1)
+
+                        response_status = self.set_question_answer(
+                            category.strip(), int(point_value.strip()))
+                        
+                        if response_status == 'ok':
+                            response = self.current_question + "\n (Type "\
+                            "'@starterbot answer:<your answer>' to answer the question)"
+                        else:
+                            response = self.get_questions_response(name)
+                    
+                    except ValueError:
+                        response = "{}, give me a command that looks something like: "\
+                        "'I'll take Python 100'".format(name)
+
+        elif command.lower().startswith(NEXT_COMMAND.lower()):
+
+
 
         elif command.lower().startswith(ANSWER_COMMAND.lower()):
-            answer = command.split(":")[1].strip().lower()
+            answer = re.split(SPLIT_REGEX_PATTERN, command)[1].strip().lower()
             print "user answer:", answer
 
-            if self.question:
-                expected_answer = self.answer.lower()
+            if self.current_question:
+                expected_answer = self.current_answer.lower()
                 print "bot answer:", expected_answer
 
                 if answer.find(expected_answer) > -1:
-                    response = "That's right! Great job."
-                    self.question = ''
-                    self.answer = ''
-                else:
-                    response = "Sorry, that's not the right answer."
-            else:
-                response = "I didn't ask a question!"
+                    if name not in self.scoreboard:
+                        self.scoreboard[name] = int(self.current_point_value)
+                    else:
+                        self.scoreboard[name] += int(self.current_point_value)
+                    self.current_question = ''
+                    self.current_answer = ''
+                    self.current_point_value = 0
 
-        elif command.lower().startswith(CATEGORIES_COMMAND.lower()):
-            response = self.get_categories_response()
+                    if len(self.available_questions) == 0:
+                        winner = self.scoreboard.iterkeys().next()
+                        for player in self.scoreboard:
+                            if self.scoreboard[winner] < self.scoreboard[player]:
+                                winner = player
+
+                        response = "That's right, {}! Great job. You answered "\
+                        " the last available question. Congrats to {} for winning "\
+                        "the game!\n Tell me 'reset' to start another game".format(name, winner)
+
+                    else:
+                        response = "That's right, {}! Great job.\n\n Tell me 'scoreboard'"\
+                        " to see who's in the lead!\n\n Use the *I'll take* command to "\
+                        " choose another question with a point value. Or you can" \
+                        " just tell me 'next' to get the next question in the same" \
+                        " same category as the last question.".format(name)
+                else:
+                    response = "Sorry, {}. {} isn't the right answer.".format(name, answer)
+            else:
+                response = "I didn't ask a question, {}!".format(name)
+
+        elif command.lower().startswith(QUESTIONS_COMMAND.lower()):
+            response = self.get_questions_response(name)
+
+        elif command.lower().startswith(SCOREBOARD_COMMAND.lower()):
+            scores = ''
+            for player, score in self.scoreboard.iteritems():
+                scores += "{}: {} points\n".format(player, score)
+
+            if scores == '':
+                response = "No one has answered any questions correctly yet."
+            else:
+                response = "Here are the current scores:\n" + scores
 
         self.slack_client.api_call("chat.postMessage", channel=channel,
                               text=response, as_user=True)
@@ -122,49 +206,23 @@ class QuestionAnswerBot(object):
                 if output and 'text' in output and AT_BOT in output['text']:
                     # return text after the @ mention, whitespace removed
                     return output['text'].split(AT_BOT)[1].strip().lower(), \
-                           output['channel']
-        return None, None
+                           output['channel'], output['user']
+        return None, None, None
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
 
-    bot = QuestionAnswerBot()
+    bot = QuizBot()
 
     if bot.slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
-            command, channel = bot.parse_slack_output(bot.slack_client.rtm_read())
-            if command and channel:
-                bot.handle_command(command, channel)
+            command, channel, user = bot.parse_slack_output(bot.slack_client.rtm_read())
+            if command and channel and user:
+                bot.handle_command(command, channel, user)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
